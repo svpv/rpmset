ba03eb4c16bb9645ea4e343a0e130f452303dbc7
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-13 03:42:17 +0300

    optimize out pmulld in 29x3
    In this particular case only one lane needs to be shifted by 4 bits,
    so we can shift by 4 and blend.  (The routine now writes garbage in
    its 4th lane, which was previously zeroed out due to multiplication
    by 0.  This is not a problem.)

--- base64unpack-sse4.h
+++ base64unpack-sse4.h
@@ -219,13 +219,14 @@ static inline void pack28x3c14(const uint32_t *v, char *s, unsigned e)
 
 static inline bool unpack28x3c14(const char *s, uint32_t *v, unsigned *e)
 {
-    __m128i x;
+    __m128i x, y;
     if (!base64unpack24(s - 2, &x)) return false;
     const __m128i shuf = _mm_setr_epi8(
 	     1,  2,  4,  5,  6,  8,  9, 10,
 	    10, 12, 13, 14, -1, -1, -1, -1);
     x = _mm_shuffle_epi8(x, shuf);
-    x = _mm_mullo_epi32(x, _mm_setr_epi32(1, 16, 1, 0));
+    y = _mm_slli_epi32(x, 4);
+    x = _mm_blend_epi16(x, y, 8 + 4);
     x = _mm_srli_epi32(x, 4);
     _mm_storeu_si128((void *) v, x);
     return (void) e, true;
