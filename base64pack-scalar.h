18a4891c375740d7d56485f38707ff715394ebe3
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-14 23:34:31 +0300

    scalar: implemented pack29x3c15e3 and unpack29x3c15e3

--- base64pack-scalar.h
+++ base64pack-scalar.h
@@ -45,6 +45,44 @@ static inline bool unpack28x3c14(const char *s, uint32_t *v, unsigned *e)
     return (void) e, true;
 }
 
+static inline void pack29x3c15e3(const uint32_t *v, char *s, unsigned e)
+{
+    s[ 0] = base64[ v[0] & Mask(6)];
+    s[ 1] = base64[(v[0] & Mask(12)) >> 6];
+    s[ 2] = base64[(v[0] & Mask(18)) >> 12];
+    s[ 3] = base64[(v[0] & Mask(24)) >> 18];
+    s[ 4] = base64[ v[1] & Mask(6)];
+    s[ 5] = base64[(v[1] & Mask(12)) >> 6];
+    s[ 6] = base64[(v[1] & Mask(18)) >> 12];
+    s[ 7] = base64[(v[1] & Mask(24)) >> 18];
+    s[ 8] = base64[ v[2] & Mask(6)];
+    s[ 9] = base64[(v[2] & Mask(12)) >> 6];
+    s[10] = base64[(v[2] & Mask(18)) >> 12];
+    s[11] = base64[(v[2] & Mask(24)) >> 18];
+    s[12] = base64[(v[0] & Mask(29)) >> 24 | (e & 1) << 5];
+    s[13] = base64[(v[1] & Mask(29)) >> 24 | (e & 2) << 4];
+    s[14] = base64[(v[2] & Mask(29)) >> 24 | (e & 4) << 3];
+}
+
+static inline bool unpack29x3c15e3(const char *s, uint32_t *v, unsigned *e)
+{
+    int32_t x;
+    uint32_t mask = (1 << 29) - 1;
+    x = base64dec2(s + 0) | base64dec2(s + 2) << 12 | base64dec1(s + 12) << 24;
+    if (x < 0) return false;
+    v[0] = x & mask;
+    *e = x >> 29;
+    x = base64dec2(s + 4) | base64dec2(s + 6) << 12 | base64dec1(s + 13) << 24;
+    if (x < 0) return false;
+    v[1] = x & mask;
+    *e |= (x >> 28) & 2;
+    x = base64dec2(s + 8) | base64dec2(s + 10) << 12 | base64dec1(s + 14) << 24;
+    if (x < 0) return false;
+    v[2] = x & mask;
+    *e |= (x >> 27) & 4;
+    return true;
+}
+
 static inline void pack30x3c15(const uint32_t *v, char *s, unsigned e)
 {
     s[ 0] = base64[ v[0] & Mask(6)];
