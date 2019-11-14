967843ef4e52d4ba076ef616f11fb67e38610f72
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-14 06:23:27 +0300

    scalar: implemented pack28x3c14 and unpack28x3c14
    I also tried 64-bit registers (filled with 60+24 bits).  On Haswell,
    it's not at all faster with gcc and marginally faster with clang.
    It seems that there's no point in using 64-bit register because
    the main difficulty is character-by-character base64 lookup.
    
        int64_t x;
        x  =           base64dec2(s + 0) | base64dec2(s + 2) << 12;
        x |= (int64_t)(base64dec2(s + 4) | base64dec2(s + 6) << 12) << 24;
        x |= (int64_t)(base64dec2(s + 8)                          ) << 48;
        if (x < 0) return false;
        v[0]  = (x & Mask64(28));
        v[1]  = (x & Mask64(56)) >> 28;
        v[2]  = (x >> 56);
        x  =           base64dec2(s + 10) | base64dec2(s + 12) << 12;
        if (x < 0) return false;
        v[2]  |= (x << 4);
        return (void) e, true;

--- base64pack-scalar.h
+++ base64pack-scalar.h
@@ -4,6 +4,47 @@
 
 #define Mask(k) ((1U << k) - 1)
 
+static inline void pack28x3c14(const uint32_t *v, char *s, unsigned e)
+{
+    s[ 0] = base64[ v[0] & Mask( 6)];
+    s[ 1] = base64[(v[0] & Mask(12)) >>  6];
+    s[ 2] = base64[(v[0] & Mask(18)) >> 12];
+    s[ 3] = base64[(v[0] & Mask(24)) >> 18];
+    s[ 4] = base64[(v[0] & Mask(28)) >> 24 |
+	           (v[1] & Mask( 2)) <<  4];
+    s[ 5] = base64[(v[1] & Mask( 8)) >>  2];
+    s[ 6] = base64[(v[1] & Mask(14)) >>  8];
+    s[ 7] = base64[(v[1] & Mask(20)) >> 14];
+    s[ 8] = base64[(v[1] & Mask(26)) >> 20];
+    s[ 9] = base64[(v[1] & Mask(28)) >> 26 |
+	           (v[2] & Mask( 4)) <<  2];
+    s[10] = base64[(v[2] & Mask(10)) >>  4];
+    s[11] = base64[(v[2] & Mask(16)) >> 10];
+    s[12] = base64[(v[2] & Mask(22)) >> 16];
+    s[13] = base64[(v[2] & Mask(28)) >> 22];
+    (void) e;
+}
+
+static inline bool unpack28x3c14(const char *s, uint32_t *v, unsigned *e)
+{
+    int32_t x;
+    x = base64dec2(s + 0) | base64dec2(s + 2) << 12;
+    if (x < 0) return false;
+    v[0]  = x;
+    x = base64dec2(s + 4) | base64dec2(s + 6) << 12;
+    if (x < 0) return false;
+    v[0] |= (x & Mask(4)) << 24;
+    v[1]  = (x >> 4);
+    x = base64dec2(s + 8) | base64dec2(s + 10) << 12;
+    if (x < 0) return false;
+    v[1] |= (x & Mask(8)) << 20;
+    v[2]  = (x >> 8);
+    x = base64dec2(s + 12);
+    if (x < 0) return false;
+    v[2] |= (x << 16);
+    return (void) e, true;
+}
+
 static inline void pack30x3c15(const uint32_t *v, char *s, unsigned e)
 {
     s[ 0] = base64[ v[0] & Mask(6)];
