903f819dad191735a624284a78f7de7e9f2b1ec6
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-14 05:20:29 +0300

    scalar: implemented pack30x3c15

--- base64pack-scalar.h
+++ base64pack-scalar.h
@@ -1,5 +1,28 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "base64.h"
+
+#define Mask(k) ((1U << k) - 1)
+
+static inline void pack30x3c15(const uint32_t *v, char *s, unsigned e)
+{
+    s[ 0] = base64[ v[0] & Mask(6)];
+    s[ 1] = base64[(v[0] & Mask(12)) >> 6];
+    s[ 2] = base64[(v[0] & Mask(18)) >> 12];
+    s[ 3] = base64[(v[0] & Mask(24)) >> 18];
+    s[ 4] = base64[ v[1] & Mask(6)];
+    s[ 5] = base64[(v[1] & Mask(12)) >> 6];
+    s[ 6] = base64[(v[1] & Mask(18)) >> 12];
+    s[ 7] = base64[(v[1] & Mask(24)) >> 18];
+    s[ 8] = base64[ v[2] & Mask(6)];
+    s[ 9] = base64[(v[2] & Mask(12)) >> 6];
+    s[10] = base64[(v[2] & Mask(18)) >> 12];
+    s[11] = base64[(v[2] & Mask(24)) >> 18];
+    s[12] = base64[(v[0] & Mask(30)) >> 24];
+    s[13] = base64[(v[1] & Mask(30)) >> 24];
+    s[14] = base64[(v[2] & Mask(30)) >> 24];
+    (void) e;
+}
 
 static inline bool unpack30x3c15(const char *s, uint32_t *v, unsigned *e)
 {
