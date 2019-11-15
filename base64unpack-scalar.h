14d15234bde8baa11dfd6224d4b4035c9512eea7
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-15 09:44:50 +0300

    scalar: implemented unpack27x3c14e3
    There was a funny bug with signed multiplication: the result could
    overflow and the right shift would shift in garbage.

--- base64unpack-scalar.h
+++ base64unpack-scalar.h
@@ -4,6 +4,28 @@
 
 #define Mask(k) ((1U << k) - 1)
 
+static inline bool unpack27x3c14e3(const char *s, uint32_t *v, unsigned *e)
+{
+    int32_t x;
+    uint32_t e0, e1, e2;
+    x = base64dec2(s + 0) | base64dec2(s + 2) << 12;
+    if (x < 0) return false;
+    v[0]  = x;
+    x = (e0 = base64dec1(s + 4)) | base64dec1shl6(s + 5) | base64dec2(s + 6) << 12;
+    if (x < 0) return false;
+    v[0] |= (x & Mask(3)) << 24;
+    v[1]  = (x >> 4);
+    x = base64dec1(s + 8) | (e1 = base64dec1shl6(s + 9)) | base64dec2(s + 10) << 12;
+    if (x < 0) return false;
+    v[1] |= (x & Mask(7)) << 20;
+    v[2]  = (x >> 8);
+    x = base64dec2(s + 12);
+    if (x < 0) return false;
+    v[2] |= (e2 = (x << 16)) & Mask(27);
+    *e = ((e0 | e1 | e2) & 0x8000088) * 0x4800010 >> 29;
+    return true;
+}
+
 static inline bool unpack28x3c14(const char *s, uint32_t *v, unsigned *e)
 {
     int32_t x;
@@ -26,8 +48,8 @@ static inline bool unpack28x3c14(const char *s, uint32_t *v, unsigned *e)
 
 static inline bool unpack29x3c15e3(const char *s, uint32_t *v, unsigned *e)
 {
-    int32_t e0, e1, e2;
     int32_t x0, x1, x2;
+    uint32_t e0, e1, e2;
     uint32_t mask = (1 << 29) - 1;
     x0 = base64dec2(s + 0) | base64dec2(s + 2) << 12 | (e0 = base64dec1(s + 12)) << 24;
     if (x0 < 0) return false;
