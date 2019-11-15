e2e75c1900e4293ef8e4d9f453f5dac70897de15
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-11-15 07:23:18 +0300

    base64pack.h: reimplemented pack27x3c14e3
    It is based on pack28x3c14.  Where there was Mask(28), there is now
    Mask(27) with one extra bit.

--- base64pack.h
+++ base64pack.h
@@ -4,6 +4,26 @@
 
 #define Mask(k) ((1U << k) - 1)
 
+static inline void pack27x3c14e3(const uint32_t *v, char *s, unsigned e)
+{
+    s[ 0] = base64[ v[0] & Mask( 6)];
+    s[ 1] = base64[(v[0] & Mask(12)) >>  6];
+    s[ 2] = base64[(v[0] & Mask(18)) >> 12];
+    s[ 3] = base64[(v[0] & Mask(24)) >> 18];
+    s[ 4] = base64[(v[0] & Mask(27)) >> 24 | (e & 1) << 3 |
+	           (v[1] & Mask( 2)) <<  4];
+    s[ 5] = base64[(v[1] & Mask( 8)) >>  2];
+    s[ 6] = base64[(v[1] & Mask(14)) >>  8];
+    s[ 7] = base64[(v[1] & Mask(20)) >> 14];
+    s[ 8] = base64[(v[1] & Mask(26)) >> 20];
+    s[ 9] = base64[(v[1] & Mask(27)) >> 26 | (e & 2) |
+	           (v[2] & Mask( 4)) <<  2];
+    s[10] = base64[(v[2] & Mask(10)) >>  4];
+    s[11] = base64[(v[2] & Mask(16)) >> 10];
+    s[12] = base64[(v[2] & Mask(22)) >> 16];
+    s[13] = base64[(v[2] & Mask(27)) >> 22 | (e & 4) << 3];
+}
+
 static inline void pack28x3c14(const uint32_t *v, char *s, unsigned e)
 {
     s[ 0] = base64[ v[0] & Mask( 6)];
