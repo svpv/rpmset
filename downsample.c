43cb8cc0cb987eb058ea3268499699140ca71951
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-03-02 10:24:39 +0300

    downsample.c: instantiate for k=1
    This is a low-hanging fruit: it reduces the average cost 2.8 -> 2.4
    cycles per integer with gcc9, 2.4 -> 1.9 with clang7 (on Haswell).
    
    I also tried to unroll the loop, but clang is already good at this,
    so I couldn't beat clang.  gcc is not as good, and unrolling the loop
    in a clever way helps a bit, 2.4 -> 2.0.  But I decided I don't want to
    interfere with the compiler in this way.  Critical loops can be written
    in assembly, but this is not a critical one.

--- downsample.c
+++ downsample.c
@@ -5,11 +5,19 @@ size_t downsample(const uint32_t *v, size_t n, uint32_t *w, int k)
     const uint32_t *v_end = v + n;
     const uint32_t *w_start = w;
     uint32_t v0 = UINT32_MAX;
-    do {
-	uint32_t v1 = *v++ >> k;
-	if (likely(v1 != v0))
-	    *w++ = v1;
-	v0 = v1;
-    } while (v < v_end);
+    if (likely(k == 1))
+	do {
+	    uint32_t v1 = *v++ >> 1;
+	    if (likely(v1 != v0))
+		*w++ = v1;
+	    v0 = v1;
+	} while (v < v_end);
+    else
+	do {
+	    uint32_t v1 = *v++ >> k;
+	    if (likely(v1 != v0))
+		*w++ = v1;
+	    v0 = v1;
+	} while (v < v_end);
     return w - w_start;
 }
