b468ce6e73df791715d2015263c54254a549baa7
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-03-02 05:48:05 +0300

    downsample by right shift
    It dawned on me that downsampling with a right shift instead of a mask
    has two crucial advantages:
    1) The order is preserved, no need to merge, so the loop becomes
    trivial (and runs 4x faster in practice).  It is somewhat similar to
    reducing precision of a floating-point number (by keeping only a few
    most significant bits of its mantissa).
    2) Any number of downsampling steps can be achieved in a single call.
    This obviates the need for dealing with double-sized buffers.
    Furthermore this makes inplace downsampling possible.
    
    The downside is that downsampling won't work with old set-versions when
    they are simply converted to the new format (which is currently my main
    testing and benchmarking method).  They have to be regenerated from
    scratch, by taking the more significant hash bits.

--- downsample.c
+++ downsample.c
@@ -1,57 +1,15 @@
 #include "rpmsetcmp-common.h"
 
-size_t downsample(const uint32_t *v, size_t n, uint32_t *w, int bpp)
+size_t downsample(const uint32_t *v, size_t n, uint32_t *w, int k)
 {
-    uint32_t mask = (1U << bpp) - 1;
-    // Find the first element with high bit set.
-    size_t l = 0;
-    size_t u = n;
-    do {
-	size_t i = (l + u) / 2;
-	if (v[i] <= mask)
-	    l = i + 1;
-	else
-	    u = i;
-    } while (l < u);
-    // Initialize parts.
+    const uint32_t *v_end = v + n;
     const uint32_t *w_start = w;
-    const uint32_t *v1 = v + 0, *v1end = v + u;
-    const uint32_t *v2 = v + u, *v2end = v + n;
-    // Merge v1 and v2 into w.
-    if (v1 < v1end && v2 < v2end) {
-	uint32_t v1val = *v1;
-	uint32_t v2val = *v2 & mask;
-	while (1) {
-	    if (v1val < v2val) {
-		*w++ = v1val;
-		v1++;
-		v1val = *v1;
-		if (v1 == v1end)
-		    break;
-	    }
-	    else if (v1val > v2val) {
-		*w++ = v2val;
-		v2++;
-		if (v2 == v2end)
-		    break;
-		v2val = *v2 & mask;
-	    }
-	    else {
-		*w++ = v1val;
-		v1++, v2++;
-		if (v2 == v2end)
-		    break;
-		v1val = *v1;
-		v2val = *v2 & mask;
-		if (v1 == v1end)
-		    break;
-	    }
-	}
-    }
-    // Append what's left.
-    while (v1 < v1end)
-	*w++ = *v1++;
-    while (v2 < v2end)
-	*w++ = *v2++ & mask;
+    uint32_t v0 = UINT32_MAX;
+    do {
+	uint32_t v1 = *v++ >> k;
+	if (likely(v1 != v0))
+	    *w++ = v1;
+	v0 = v1;
+    } while (v < v_end);
     return w - w_start;
 }
