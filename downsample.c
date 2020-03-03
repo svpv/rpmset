4e55aee48b459663b247b814223383c029bfccb8
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-03-03 05:55:20 +0300

    downsample.c: unroll the loop
    I've changed my mind, I'm actually able to beat clang by a small margin
    (1.77 -> 1.74 cycles per integer) and gcc by a big margin (2.5 -> 1.8).

--- downsample.c
+++ downsample.c
@@ -2,22 +2,36 @@
 
 size_t downsample(const uint32_t *v, size_t n, uint32_t *w, int k)
 {
-    const uint32_t *v_end = v + n;
+    const uint32_t *v_last = v + n - 1;
     const uint32_t *w_start = w;
-    uint32_t v0 = UINT32_MAX;
+    uint32_t v0, v1 = UINT32_MAX;
+    if (unlikely(v == v_last))
+	goto last;
     if (likely(k == 1))
 	do {
-	    uint32_t v1 = *v++ >> 1;
+	    v0 = v[0] >> 1;
+	    if (likely(v0 != v1))
+		*w++ = v0;
+	    v1 = v[1] >> 1;
 	    if (likely(v1 != v0))
 		*w++ = v1;
-	    v0 = v1;
-	} while (v < v_end);
+	    v += 2;
+	} while (v < v_last);
     else
 	do {
-	    uint32_t v1 = *v++ >> k;
+	    v0 = v[0] >> k;
+	    if (likely(v0 != v1))
+		*w++ = v0;
+	    v1 = v[1] >> k;
 	    if (likely(v1 != v0))
 		*w++ = v1;
-	    v0 = v1;
-	} while (v < v_end);
+	    v += 2;
+	} while (v < v_last);
+    if (v == v_last) {
+last:
+	v0 = v[0] >> k;
+	if (likely(v0 != v1))
+	    *w++ = v0;
+    }
     return w - w_start;
 }
