122018882e01c360040ffccee9b6668dcf847f67
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-08-16 07:08:24 +0300

    conv.c: increase bpp with +1
    I need this to profile the decoding routine for m=11.  The order in v[]
    is preserved, the original value is shifted v[i] << k, with k low
    pseudo-random bits generated from v[i].  This won't work properly with
    downsampling, but downsmapling doesn't work with the old data anyway
    (because I changed the downsampling scheme to strip LSB instead of MSB).

--- conv.c
+++ conv.c
@@ -30,13 +30,41 @@ void recheck(const char *ss, size_t len, const uint32_t *v, size_t n)
     free(w);
 }
 
-void conv(const char *s, size_t len, char nl)
+uint32_t mix(uint32_t h)
+{
+    h ^= h >> 16;
+    h *= 0x85ebca6b;
+    h ^= h >> 13;
+    h *= 0xc2b2ae35;
+    h ^= h >> 16;
+    return h;
+}
+
+void genlo(uint32_t *v, size_t n, int incbpp)
+{
+    uint32_t mask = (1 << incbpp) - 1;
+    for (size_t i = 0; i < n; i++) {
+	uint32_t lo = mix(v[i]) & mask;
+	v[i] = v[i] << incbpp | lo;
+    }
+}
+
+void conv(const char *s, size_t len, char nl, int incbpp)
 {
     assert(strncmp(s, "set:", 4) == 0);
     s += 4;
     size_t n;
     int bpp, m;
     uint32_t *v = decode(s, len, &n, &bpp);
+    if (incbpp) {
+	bpp += incbpp;
+	if (bpp > 32) {
+	    incbpp = bpp - 32;
+	    bpp = 32;
+	}
+	if (incbpp)
+	    genlo(v, n, incbpp);
+    }
     size_t len1 = setstring_encinit(v, n, bpp, &m);
     assert(len1 > 0);
     char *ss = malloc(len1 + 4);
@@ -44,7 +72,7 @@ void conv(const char *s, size_t len, char nl)
     size_t len2 = setstring_encode(v, n, bpp, m, ss + 4);
     assert(len2 > 0);
     assert(len2 < len1); // len1 accounts for '\0', len2 does not
-    assert(len2 <= len);
+    assert(len2 <= len || incbpp);
     assert(strlen(ss + 4) == len2);
     recheck(ss + 4, len2, v, n);
     ss[4+len2] = nl;
@@ -56,8 +84,16 @@ void conv(const char *s, size_t len, char nl)
 
 int main(int argc, const char **argv)
 {
+    int incbpp = 0;
+    if (argc > 1 && argv[1][0] == '+') {
+	char c = argv[1][1];
+	assert(c >= '1' && c <= '9');
+	assert(argv[1][2] == '\0');
+	incbpp = c - '0';
+	argc--, argv++;
+    }
     if (argc == 2) {
-	conv(argv[1], strlen(argv[1]), '\n');
+	conv(argv[1], strlen(argv[1]), '\n', incbpp);
 	return 0;
     }
     assert(argc == 1);
@@ -75,11 +111,11 @@ int main(int argc, const char **argv)
 	}
 	*s2 = line[len] = '\0';
 	if (s2 == line + len)
-	    conv(line, len, '\n');
+	    conv(line, len, '\n', incbpp);
 	else {
-	    conv(line, s2 - line, sep);
+	    conv(line, s2 - line, sep, incbpp);
 	    s2++;
-	    conv(s2, line + len - s2, '\n');
+	    conv(s2, line + len - s2, '\n', incbpp);
 	}
     }
     free(line);
